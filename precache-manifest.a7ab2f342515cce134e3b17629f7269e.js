9215ca78046b39befc4a620ee8064dbf6c7dcc7e
raghulmakeit <raghulmakeit@gmail.com>
2020-07-10 20:49:03 +0530

    Updates

---
+++ precache-manifest.a7ab2f342515cce134e3b17629f7269e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599dccd803ca6e506ba5d02941733336",
+    "url": "/sijiide/index.html"
+  },
+  {
+    "revision": "66d15a7d72b4770666a1",
+    "url": "/sijiide/static/css/2.5eaa5b7c.chunk.css"
+  },
+  {
+    "revision": "66d15a7d72b4770666a1",
+    "url": "/sijiide/static/js/2.9436b107.chunk.js"
+  },
+  {
+    "revision": "e6b73a2f5280b06c64be",
+    "url": "/sijiide/static/js/main.62997b65.chunk.js"
+  },
+  {
+    "revision": "8270af3b90f9d2df2a79",
+    "url": "/sijiide/static/js/runtime-main.a23fe33a.js"
+  },
+  {
+    "revision": "bccc802c8f6c1a79091f938b8748051c",
+    "url": "/sijiide/static/media/computer.bccc802c.jpg"
+  }
+]);
---
+++ static/js/main.62997b65.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmy_ide=this.webpackJsonpmy_ide||[]).push([[0],{63:function(p,s,e){p.exports=e(89)}},[[63,1,2]]]);
+//# sourceMappingURL=main.62997b65.chunk.js.map
